496351468d30f7e55d39a0a06124d16d89a0d2c7
zedu3 <zelalem138@gmail.com>
2021-10-16 23:10:15 -0700

    printing a quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
 int main(void)
 {
-  printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-  return (0);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n");
+	return (0);
 }
