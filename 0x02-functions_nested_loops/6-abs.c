bc2fb069cc60c01ea8f6b1aba7caf6b44fd71182
zedu3 <zelalem138@gmail.com>
2021-10-20 11:12:08 -0700

    working with absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *
+ *_abs - prints an absolute number
+ *@int: parameter
+ *Returns: always positive
+ */
+int _abs(int)
+{
+	int a;
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
