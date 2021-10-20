d61782443015dc383dd3a29f93ba97b89c3a4ec7
zedu3 <zelalem138@gmail.com>
2021-10-20 06:04:58 -0700

    printing 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabet in 10 rows
+ */
+void print_alphabet_x10(void);
+{
+	int i = 0;
+	while( i<10)
+	{
+		char j = 'a';
+		while( j<='z')
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
