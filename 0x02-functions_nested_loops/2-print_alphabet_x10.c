3873ecbd22245e1955af429250e9f28caf4b5cd7
zedu3 <zelalem138@gmail.com>
2021-10-20 06:17:38 -0700

    alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,30 @@
 #include "main.h"
+
+/**
+ * print_alphabet - prints lower case alphabet
+ *
+ **/
+
+void print_alphabet(void)
+{
+
+        char lower_case_letter = 'a';
+
+        while (lower_case_letter <= 'z')
+        {
+                _putchar(lower_case_letter);
+                lower_case_letter++;
+        }
+        _putchar('\n');
+}
+
 /**
  *print_alphabet_x10 - prints alphabet in 10 rows
  */
+
 void print_alphabet_x10(void);
 {
 	int i = 0;
-	while( i<10)
-	{
-		char j = 'a';
-		while( j<='z')
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
-	}
+	while (i<10)
+		print_alphabet();
 }
