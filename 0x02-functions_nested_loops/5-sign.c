94bc7f480dcd46e2152abb89bdf27e53932af45f
zedu3 <zelalem138@gmail.com>
2021-10-20 10:13:56 -0700

    done with neg pos

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_sign - tells weather the number is negative or positive
+ *@n: the number to be evaluated
+ *Return: returns 1 if positive 0 if zero -1 if negative
+ *
+ */
+int print_sign(int n)
+{
+	while (n < 0)
+		return (-1);
+	while (n > 0)
+		return (1);
+	while (n = 0)
+		return (0);
+}
