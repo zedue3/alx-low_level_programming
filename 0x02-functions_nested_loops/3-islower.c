266a8fd292cc19cbc75d277b942b6f19550c14c5
zedu3 <zelalem138@gmail.com>
2021-10-20 07:16:40 -0700

    working with the lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
  *_islower - returns 1 if lowercase present
- *
+ *c - is a parameter to bechecked
+ *Return - 1 if lowercase 0 otherwise
  */
 int _islower(int c)
 {
-	while (c >= 97 && c <=122)
+	while (c >= 97 && c <= 122)
 		return (1);
 	return (0);
 }
