ae926921aca8f5553a37b3083160d8eda76ceefd
zedu3 <zelalem138@gmail.com>
2021-10-20 10:50:55 -0700

    done with printing positive and negative

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,29 @@
-#include "main.h"
+#include "holberton.h"
+
+
 /**
- *print_sign - tells weather the number is negative or positive
- *@n: the number to be evaluated
- *Return: returns 1 if positive 0 if zero -1 if negative
+ * print_sign - determin sign of int
  *
- */
+ * @n: number to check for sign
+ *
+ * Return: 1 if positive, 0 if 0, and -1 if negative
+ **/
+
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
-	else (n > 0)
+	else
 	{
-		_putchar('1');
-		return (1);
+		_putchar('-');
+		return (-1);
 	}
-
 }
