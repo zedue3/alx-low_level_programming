8100387aff9f1f4968de7498a9bd6e584c45a12d
zedu3 <zelalem138@gmail.com>
2021-10-20 10:04:56 -0700

    done wit is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *
- *
- *
+ *_isalpha - returns 1 if alphabet, 0 otherwise
+ *@c: which is inside the pharentesis
+ *Return: if letter is present 1, otherwise 0
  */
 int _isalpha(int c)
 {
@@ -10,5 +10,5 @@ int _isalpha(int c)
 		return (1);
 	while (c >= 97 && c <= 122)
 		return (1);
-	return(0);
+	return (0);
 }
