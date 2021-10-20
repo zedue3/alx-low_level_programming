5c19139844184df1958ef58fa6806f5241997a2c
zedu3 <zelalem138@gmail.com>
2021-10-20 05:15:02 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,15 @@
 /**
  *main - check the code
  *Description - prints alphabet with lower case
- *Return: always 0
+ *
  */
 
 void print_alphabet(void)
 {
 	char i;
-	for(i='a'; i<='z'; i++){
+	for( i = 'a'; i <= 'z'; i++)
+	
 		_putchar(i);
-	}
+	
 	_putchar('\n');
 }
