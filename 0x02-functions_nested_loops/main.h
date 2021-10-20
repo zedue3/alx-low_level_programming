ec21135db4c5a0a6ca92a7bb52a5979d061988be
zedu3 <zelalem138@gmail.com>
2021-10-20 04:45:37 -0700

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,10 @@
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 
 int _putchar(char);
-void print_alphabet(void);
+void print_alphabet(void)
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
