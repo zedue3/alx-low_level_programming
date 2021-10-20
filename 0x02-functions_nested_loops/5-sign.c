c540873e9a0a13dc84c39d36242afab54eed6498
zedu3 <zelalem138@gmail.com>
2021-10-20 10:36:48 -0700

    done with printing positive and negative

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,12 +8,18 @@
 int print_sign(int n)
 {
 	while (n < 0)
+	{
 		_putchar('-');
 		return (-1);
+	}
 	while (n > 0)
+	{
 		_putchar('+');
 		return (1);
+	}
 	while (n == 0)
+	{
 		_putchar('0');
 		return (0);
+	}
 }
