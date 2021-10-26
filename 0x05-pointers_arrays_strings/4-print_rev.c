e304a1f0c45180e790c1fa42fb82a5360c853dee
zedu3 <zelalem138@gmail.com>
2021-10-26 05:17:47 -0700

    reversing

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
+	_putchar('\n');
+}
