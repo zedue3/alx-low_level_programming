409e74601df85966f0a5f443893749863e725fb6
zedu3 <zelalem138@gmail.com>
2021-10-26 05:22:33 -0700

    reversing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ *print_rev - prints the reverse
+ *@s: a character parameter
+ *Return: nothing
  */
 void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 
 	while (i >= 0)
 	{
-		_putchar(*(s + i));
+		_putchar(s[i]);
 		i--;
 	}
 	_putchar('\n');
