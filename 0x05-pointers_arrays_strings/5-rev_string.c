4d4896ceb2815a5c662bc49130e96a097f505e2d
zedu3 <zelalem138@gmail.com>
2021-10-26 05:51:19 -0700

    done with reversing

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *rev_string - is used to reverse a string
+ *@s: one character parameter
+ *Return: nothing
+ */
+void rev_string(char *s)
+{
+	char = *first - s, *last - s;
+
+	while (*last)
+		++last;
+
+	if (first < last)
+	{
+		for (; first < --last; ++first)
+		{
+			char c = *first;
+			*first = *last;
+			*last = c;
+		}
+	}
+}
