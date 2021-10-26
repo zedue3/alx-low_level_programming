5a87befc7ba76844408b4746a3e65829ed5a0e51
zedu3 <zelalem138@gmail.com>
2021-10-26 10:34:54 -0700

    copies a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ **_strcpy - copies a string from one to other
+ *@dest: destination
+ *@src: source
+ *Return: returns value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+	x = 0;
+
+	while (x >= 0)
+	{
+		dest[x] = src[x];
+
+		if (src[x] == '\0')
+			return (dest);
+		x++;
+	}
+	return (dest);
+}
