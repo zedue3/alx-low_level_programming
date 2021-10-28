9b3e35db61f482e1a9312d0b3019dac527c965be
zedu3 <zelalem138@gmail.com>
2021-10-28 04:14:22 -0700

    concatinating

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strcat - concatinates strings togeteher
+ *@dest: is destination
+ *@src: is source
+ *Return: returns the destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+	i = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; src[j] != 0; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = src[j];
+
+	return (dest);
