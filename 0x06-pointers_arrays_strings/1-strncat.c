0f2f8a479aab410566801ef719378bc99a23447e
zedu3 <zelalem138@gmail.com>
2021-10-28 05:54:00 -0700

    concatenate with n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strncat - concatinates source with destination by the value of n
+ *@dest: is destination
+ *@src: is source
+ *@n: is integer
+ *Return: returns the concatinated value
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j;
+
+	while (dest[i] != '\0')
+		i++;
+
+	for (j = 0; j != n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	if (dest[i - 1] != '\0')
+		dest[i] = '\0';
+	return (dest);
+}
