5dec80ddf046be3f41e1641d93a86cbace29e34b
zedu3 <zelalem138@gmail.com>
2021-10-28 06:02:47 -0700

    done with concatenation

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,8 +6,8 @@
  *@src: is source
  *@n: is integer
  *Return: returns the concatinated value
+ *Description - used to concatenat
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; j < n; j++)
+	for (j = 0; j != n; j++)
 	{
 		dest[i] = src[j];
 		i++;
