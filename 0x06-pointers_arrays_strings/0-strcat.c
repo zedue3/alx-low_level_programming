02e38ea1c76a457d9eb34312e8565035bae27218
zedu3 <zelalem138@gmail.com>
2021-10-28 04:26:10 -0700

    concatinate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 	int j;
-	i = 0;
 
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; src[j] != 0; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
