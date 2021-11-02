e89cc9ee9909f109762c6deb3cdf13a9097a6ee8
zedu3 <zelalem138@gmail.com>
2021-11-02 10:25:36 -0700

    all in one

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,12 @@
 #include "main.h"
+/**
+ *_memset - is a signiture
+ *@s: is one parameter from the memeset
+ *@b: second parameter
+ *@n: integer arameter
+ *Return: returns the sorted s
+ *
+ **/
 char *_memset(char *s, char b, unsigned int n)
 {
 	for (; n > 0; n--)
