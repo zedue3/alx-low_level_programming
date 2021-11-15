c338b16f0dcd1ada1f41a8d23be7cf97f1084b57
zedu3 <zelalem138@gmail.com>
2021-11-15 01:47:04 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,13 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-        int i = 0;
+	int i = 0;
 
-        if (s[i] != '\0')
-        {
-                _putchar(s[i]);
-                _puts_recursion(&s[i + 1]);
-        }
-        else
-                _putchar('\n');
+	if (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		_puts_recursion(&s[i + 1]);
+	}
+	else
+		_putchar('\n');
 }
